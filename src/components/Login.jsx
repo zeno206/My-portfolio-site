5e0271d9ef3ed500e14e0b463bd6cee2200e0cfc
shubhamdhiman <shubham.dhiman.mehra@gmail.com>
2026-04-27 23:02:50 +0530

    update frontend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,41 @@
 import React from "react";
+import axios from "axios";
 
 function Login() {
+  const login = (e) => {
+    e.preventDefault();
+
+    const formdata = new FormData(e.target);
+    const logininfo = {
+      email: formdata.get("email"),
+      password: formdata.get("password"),
+    };
+    
+
+      const log = await axios.post(
+        "https://webiste-backend-q9b4.onrender.com/api/post/login",
+        logininfo,
+      ).then((res)=>{
+   console.log(res.data)
+      }).catch((error)=>{
+    console.log(error)
+      })
+  };
   return (
     <>
       <div className="lgndiv">
         <div className="lgn-box">
-          <form className="lgn-frm">
+          <form className="lgn-frm" onSubmit={login}>
             <h3 className="frm-h3">Welcome to my Portfolio</h3>
             <p className="frm-h2">Sign in or create your account to started</p>
             <h1 className="h-btn">Login👋</h1>
             <label htmlFor=""> Email</label>
             <br />
-            <input type="text" placeholder="Enter your email" />
+            <input type="text" placeholder="Enter your email" name="email" />
             <br />
             <label>Password</label>
             <br />
-            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password" name="password" />
             <br />
             <button className="lgn-btn">login</button>
           </form>
